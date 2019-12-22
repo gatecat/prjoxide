c8c8f469a3fd05f257e6dabbf70c7fd9aba5c8ac
David Shah <dave@ds0.me>
2019-12-22 17:34:07 +0000

    Edge wirename handling from Trellis
    Signed-off-by: David Shah <dave@ds0.me>

--- libprjoxide/src/wires.rs
+++ libprjoxide/src/wires.rs
@@ -24,6 +24,16 @@ lazy_static! {
     static ref ECLK_MUXIN_RE: Regex = Regex::new(r"^JMUXIN(\d+)_ECLKBANK_CORE_ECLKBANK(\d+)$").unwrap();
     // DQS group shared signals
     static ref DQS_GROUP_RE: Regex = Regex::new(r"^J(WRPNTR\d|RDPNTR\d|DQSR90|DQSW270|DQSW)_DQSBUF_CORE_I_DQS_TOP$").unwrap();
+
+    // - CIB (general routing) regex
+    static ref GENERAL_ROUTE_RE: Regex = Regex::new(r"R\d+C\d+_[VH]\d{2}[NESWTLBR]\d{4}").unwrap();
+    static ref CIB_SIG_RE: Regex = Regex::new(r"R\d+C\d+_J?(CIBMUXOUT|CIBMUXIN)?[ABCDMFQ]\d").unwrap();
+    static ref CIB_CTRLSIG_RE: Regex = Regex::new(r"R\d+C\d+_J?(CIBMUXOUT|CIBMUXIN)?(CLK|LSR|CE)\d").unwrap();
+    static ref CIB_BOUNCE_RE: Regex = Regex::new(r"R\d+C\d+_[NESW]BOUNCE").unwrap();
+
+    static ref H_WIRE_RE: Regex = Regex::new(r"^H(\d{2})([EW])(\d{2})(\d{2})$").unwrap();
+    static ref V_WIRE_RE: Regex = Regex::new(r"^V(\d{2})([NS])(\d{2})(\d{2})$").unwrap();
+
 }
 
 fn is_full_global_wn(wire: &str) -> bool {
@@ -32,6 +42,177 @@ fn is_full_global_wn(wire: &str) -> bool {
         || GLB_MIDMUX_RE.is_match(wire)
         || ECLK_RE.is_match(wire)
         || ECLK_MUXIN_RE.is_match(wire)
+}
+
+pub fn handle_edge_name(
+    max_x: u32,
+    max_y: u32,
+    tx: u32,
+    ty: u32,
+    wx: u32,
+    wy: u32,
+    wn: &str,
+) -> (String, u32, u32) {
+    /*
+    At the edges of the device, canonical wire names do not follow normal naming conventions, as they
+    would mean the nominal position of the wire would be outside the bounds of the chip. Before we add routing to the
+    database, we must however normalise these names to what they would be if not near the edges, otherwise there is a
+    risk of database conflicts, having multiple names for the same wire.
+
+    Returns a tuple (netname, x, y)
+    */
+    if let Some(hm) = H_WIRE_RE.captures(wn) {
+        match &hm[1] {
+            "01" => {
+                // H01xyy00 --> x+1, H01xyy01
+                if tx == max_x - 1 {
+                    assert_eq!(hm[4].to_string(), "00");
+                    return (format!("H01{}{}01", &hm[2], &hm[3]), wx + 1, wy);
+                }
+            }
+            "02" => {
+                if tx == 1 {
+                    // H02E0002 --> x-1, H02E0001
+                    // H02W0000 --> x-1, H02W00001
+                    if &hm[2] == "E" && wx == 1 && &hm[4] == "02" {
+                        return (format!("H02E{}01", &hm[3]), wx - 1, wy);
+                    } else if &hm[2] == "W" && wx == 1 && &hm[4] == "00" {
+                        return (format!("H02W{}01", &hm[3]), wx - 1, wy);
+                    }
+                } else if tx == max_x - 1 {
+                    // H02E0000 --> x+1, H02E0001
+                    // H02W0002 --> x+1, H02W00001
+                    if &hm[2] == "E" && wx == max_x - 1 && &hm[4] == "00" {
+                        return (format!("H02E{}01", &hm[3]), wx + 1, wy);
+                    } else if &hm[2] == "W" && wx == max_x - 1 && &hm[4] == "02" {
+                        return (format!("H02W{}01", &hm[3]), wx + 1, wy);
+                    }
+                }
+            }
+            "06" => {
+                if tx <= 5 {
+                    // x-2, H06W0302 --> x-3, H06W0303
+                    // x-2, H06E0004 --> x-3, H06E0003
+                    // x-1, H06W0301 --> x-3, H06W0303
+                    // x-1, H06E0305 --> x-3, H06E0303
+                    match &hm[2] {
+                        "W" => {
+                            return (
+                                format!("H06W{}03", &hm[3]),
+                                wx - (3 - hm[4].parse::<u32>().unwrap()),
+                                wy,
+                            )
+                        }
+                        "E" => {
+                            return (
+                                format!("H06E{}03", &hm[3]),
+                                wx - (hm[4].parse::<u32>().unwrap() - 3),
+                                wy,
+                            )
+                        }
+                        _ => panic!("unknown H06 wire {}", wn),
+                    }
+                } else if tx >= max_x - 5 {
+                    match &hm[2] {
+                        "W" => {
+                            return (
+                                format!("H06W{}03", &hm[3]),
+                                wx + (hm[4].parse::<u32>().unwrap() - 3),
+                                wy,
+                            )
+                        }
+                        "E" => {
+                            return (
+                                format!("H06E{}03", &hm[3]),
+                                wx + (3 - hm[4].parse::<u32>().unwrap()),
+                                wy,
+                            )
+                        }
+                        _ => panic!("unknown H06 wire {}", wn),
+                    }
+                }
+            }
+            _ => panic!("bad HWIRE {}", &wn),
+        }
+    }
+    if let Some(vm) = V_WIRE_RE.captures(wn) {
+        match &vm[1] {
+            "01" => {
+                if ty == 1 {
+                    if wy == 1 && &vm[2] == "N" && &vm[4] == "00" {
+                        return (format!("V01{}{}01", &vm[2], &vm[3]), wx, wy - 1);
+                    }
+                    if wy == 1 && &vm[2] == "S" && &vm[4] == "01" {
+                        return (format!("V01{}{}01", &vm[2], &vm[3]), wx, wy - 1);
+                    }
+                }
+            }
+            "02" => {
+                if ty == 1 {
+                    if &vm[2] == "S" && wy == 1 && &vm[4] == "02" {
+                        return (format!("V02S{}01", &vm[3]), wx, wy - 1);
+                    }
+                    if &vm[2] == "N" && wy == 1 && &vm[4] == "00" {
+                        return (format!("V02N{}01", &vm[3]), wx, wy - 1);
+                    }
+                } else if ty == max_y - 1 {
+                    if &vm[2] == "S" && wy == (max_y - 1) && &vm[4] == "00" {
+                        return (format!("V02S{}01", &vm[3]), wx, wy + 1);
+                    }
+                    if &vm[2] == "N" && wy == (max_y - 1) && &vm[4] == "02" {
+                        return (format!("V02N{}01", &vm[3]), wx, wy + 1);
+                    }
+                }
+            }
+            "06" => {
+                if ty <= 5 {
+                    // y-2, V06N0302 --> y-3, H06W0303
+                    // y-2, V06S0004 --> y-3, V06S0003
+                    // y-1, V06N0301 --> y-3, V06N0303
+                    // y-1, V06S0005 --> y-3, V06S0003
+                    match &vm[2] {
+                        "N" => {
+                            return (
+                                format!("V06N{}03", &vm[3]),
+                                wx,
+                                wy - (3 - vm[4].parse::<u32>().unwrap()),
+                            )
+                        }
+                        "S" => {
+                            return (
+                                format!("V06S{}03", &vm[3]),
+                                wx,
+                                wy - (vm[4].parse::<u32>().unwrap() - 3),
+                            )
+                        }
+                        _ => panic!("unknown V06 wire {}", wn),
+                    }
+                } else if ty >= max_y - 5 {
+                    // y+2, V06N0304 --> y+3, V06N0303
+                    // y+2, V06S0302 --> x+3, V06S0303
+                    match &vm[2] {
+                        "N" => {
+                            return (
+                                format!("V06N{}03", &vm[3]),
+                                wx,
+                                wy + (vm[4].parse::<u32>().unwrap() - 3),
+                            )
+                        }
+                        "S" => {
+                            return (
+                                format!("V06S{}03", &vm[3]),
+                                wx,
+                                wy + (3 - vm[4].parse::<u32>().unwrap()),
+                            )
+                        }
+                        _ => panic!("unknown V06 wire {}", wn),
+                    }
+                }
+            }
+            _ => panic!("bad VWIRE {}", &wn),
+        }
+    }
+    (wn.to_string(), wx, wy)
 }
 
 pub fn normalize_wire(chip: &Chip, tile: &Tile, wire: &str) -> String {
@@ -68,11 +249,14 @@ pub fn normalize_wire(chip: &Chip, tile: &Tile, wire: &str) -> String {
     let spw = WIRE_RE
         .captures(wire)
         .expect(&format!("invalid wire name '{}'", wire));
-    let (wy, wx, wn) = (
+    let (mut wy, mut wx, mut wn) = (
         spw[1].parse::<u32>().unwrap(),
         spw[2].parse::<u32>().unwrap(),
         &spw[3],
     );
+    if wn.ends_with("VCCHPRX") || wn.ends_with("VCCHPBX") || wn.ends_with("VCC") {
+        return "G:VCC".to_string();
+    }
     if tile.name.contains("TAP") && wn.starts_with("H") {
         if wx < tile.x {
             return format!("BRANCH_L:{}", wn);
@@ -93,6 +277,18 @@ pub fn normalize_wire(chip: &Chip, tile: &Tile, wire: &str) -> String {
     } else if DQS_GROUP_RE.is_match(wn) {
         return format!("DQSG:{:}", wn);
     }
+    let en = handle_edge_name(
+        chip.data.max_col,
+        chip.data.max_row,
+        tile.x,
+        tile.y,
+        wx,
+        wy,
+        wn,
+    );
+    wn = &en.0;
+    wx = en.1;
+    wy = en.2;
     if wx == tile.x && wy == tile.y {
         return wn.to_string();
     } else {
