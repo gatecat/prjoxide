8d28f866109223abedf6cac88c44234061bcd7f5
David Shah <dave@ds0.me>
2019-12-24 09:48:58 +0000

    Fix missing PLC pips
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/LIFCL/001-plc-routing/fuzzer.py
+++ fuzzers/LIFCL/001-plc-routing/fuzzer.py
@@ -9,7 +9,17 @@ def main():
     r = 16
     c = 22
     nodes = ["R{}C{}_J*".format(r, c)]
-    fuzz_interconnect(config=cfg, nodenames=nodes, regex=True, bidir=True)
+    extra_sources = []
+    extra_sources += ["R{}C{}_H02E{:02}01".format(r, c+1, i) for i in range(8)]
+    extra_sources += ["R{}C{}_H06E{:02}03".format(r, c+3, i) for i in range(4)]
+    extra_sources += ["R{}C{}_V02N{:02}01".format(r-1, c, i) for i in range(8)]	
+    extra_sources += ["R{}C{}_V06N{:02}03".format(r-3, c, i) for i in range(4)]	
+    extra_sources += ["R{}C{}_V02S{:02}01".format(r+1, c, i) for i in range(8)]	
+    extra_sources += ["R{}C{}_V06S{:02}03".format(r+3, c, i) for i in range(4)]	
+    extra_sources += ["R{}C{}_H02W{:02}01".format(r, c-1, i) for i in range(8)]
+    extra_sources += ["R{}C{}_H06W{:02}03".format(r, c-3, i) for i in range(4)]
+    fuzz_interconnect(config=cfg, nodenames=nodes, regex=True, bidir=True, ignore_tiles=set(["TAP_PLC_R16C14:TAP_PLC"]))
+    fuzz_interconnect(config=cfg, nodenames=extra_sources, regex=False, bidir=False, ignore_tiles=set(["TAP_PLC_R16C14:TAP_PLC"]))
 
 if __name__ == "__main__":
     main()
