4fe1395994813522fe8740e45bdab95da2fbe567
David Shah <dave@ds0.me>
2019-12-24 11:00:41 +0000

    Improve interconnect fuzzing
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/LIFCL/030-io_route/fuzzer.py
+++ fuzzers/LIFCL/030-io_route/fuzzer.py
@@ -9,9 +9,12 @@ def main():
     r = 56
     c = 8
     nodes = ["R{}C{}_*".format(r, c)]
-    def nodename_filter(x):
-        return "_GEARING_PIC_TOP_" in x or "SEIO18_CORE" in x or "DIFFIO18_CORE" in x or "I217" in x or "I218" in x
-    fuzz_interconnect(config=cfg, nodenames=nodes, regex=True, bidir=True)
+    def nodename_filter(x, nodes):
+        return ("R{}C{}_".format(r, c) in x) and ("_GEARING_PIC_TOP_" in x or "SEIO18_CORE" in x or "DIFFIO18_CORE" in x or "I217" in x or "I218" in x)
+    def pip_filter(pip, nodes):
+    	from_wire, to_wire = pip
+    	return not ("ADC_CORE" in to_wire or "ECLKBANK_CORE" in to_wire or "MID_CORE" in to_wire or "REFMUX_CORE" in to_wire)
+    fuzz_interconnect(config=cfg, nodenames=nodes, nodename_predicate=nodename_filter, pip_predicate=pip_filter, regex=True, bidir=True)
 
 if __name__ == "__main__":
     main()
--- util/fuzz/interconnect.py
+++ util/fuzz/interconnect.py
@@ -41,38 +41,37 @@ def fuzz_interconnect(
     base_bitf = config.build_design(config.sv, {"arcs_attr": ""}, "base_")
 
     all_wirenames = set([n.name for n in nodes])
-
-    def per_node(node):
+    all_pips = set()
+    for node in nodes:
+        for p in node.uphill_pips:
+            all_pips.add((p.from_wire, p.to_wire))
+        if bidir:
+            for p in node.downhill_pips:
+                all_pips.add((p.from_wire, p.to_wire))
+    per_sink = list(sorted(all_pips))
+    # First filter using netname predicate
+    if nodename_filter_union:
+        all_pips = filter(lambda x: nodename_predicate(x[0], all_wirenames) and nodename_predicate(x[1], all_wirenames),
+                            all_pips)
+    else:
+        all_pips = filter(lambda x: nodename_predicate(x[0], all_wirenames) or nodename_predicate(x[1], all_wirenames),
+                            all_pips)
+    # Then filter using the pip predicate
+    fuzz_pips = list(filter(lambda x: pip_predicate(x, all_wirenames), all_pips))
+    if len(fuzz_pips) == 0:
+        return
+    sinks = {}
+    for from_wire, to_wire in fuzz_pips:
+        if to_wire not in sinks:
+            sinks[to_wire] = []
+        sinks[to_wire].append(from_wire)
+    def per_sink(to_wire):
         # Get a unique prefix from the thread ID
         prefix = "thread{}_".format(threading.get_ident())
-        assoc_pips = set()
-        for p in node.uphill_pips:
-        	assoc_pips.add((p.from_wire, p.to_wire))
-        if bidir:
-        	for p in node.downhill_pips:
-        		assoc_pips.add((p.from_wire, p.to_wire))
-        assoc_pips = list(sorted(assoc_pips))
-        # First filter using netname predicate
-        if nodename_filter_union:
-            assoc_pips = filter(lambda x: nodename_predicate(x[0], all_wirenames) and nodename_predicate(x[1], all_wirenames),
-                                assoc_pips)
-        else:
-            assoc_pips = filter(lambda x: nodename_predicate(x[0], all_wirenames) or nodename_predicate(x[1], all_wirenames),
-                                assoc_pips)
-        # Then filter using the pip predicate
-        fuzz_pips = list(filter(lambda x: pip_predicate(x, all_wirenames), assoc_pips))
-        if len(fuzz_pips) == 0:
-        	return
-        sinks = {}
-        for from_wire, to_wire in fuzz_pips:
-        	if to_wire not in sinks:
-        		sinks[to_wire] = []
-        	sinks[to_wire].append(from_wire)
-        for to_wire in sorted(sinks.keys()):
-        	fz = libprjoxide.Fuzzer.pip_fuzzer(fuzzconfig.db, base_bitf, set(config.tiles), to_wire, config.tiles[0], ignore_tiles, full_mux_style, False)
-        	for from_wire in sinks[to_wire]:
-        		arcs_attr = r', \dm:arcs ="{}.{}"'.format(to_wire, from_wire)
-        		arc_bit = config.build_design(config.sv, {"arcs_attr": arcs_attr}, prefix)
-        		fz.add_pip_sample(fuzzconfig.db, from_wire, arc_bit)
-        	fz.solve(fuzzconfig.db)
-    fuzzloops.parallel_foreach(nodes, per_node)
+        fz = libprjoxide.Fuzzer.pip_fuzzer(fuzzconfig.db, base_bitf, set(config.tiles), to_wire, config.tiles[0], ignore_tiles, full_mux_style, False)
+        for from_wire in sinks[to_wire]:
+            arcs_attr = r', \dm:arcs ="{}.{}"'.format(to_wire, from_wire)
+            arc_bit = config.build_design(config.sv, {"arcs_attr": arcs_attr}, prefix)
+            fz.add_pip_sample(fuzzconfig.db, from_wire, arc_bit)
+        fz.solve(fuzzconfig.db)
+    fuzzloops.parallel_foreach(list(sorted(sinks.keys())), per_sink)
