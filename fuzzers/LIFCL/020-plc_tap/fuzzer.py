b1f33489e9046743f4d4fd7bddb554fc4e2e8399
David Shah <dave@ds0.me>
2020-01-01 11:07:05 +0000

    Fuzz all TAP location types
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/LIFCL/020-plc_tap/fuzzer.py
+++ fuzzers/LIFCL/020-plc_tap/fuzzer.py
@@ -2,12 +2,22 @@ from fuzzconfig import FuzzConfig
 from interconnect import fuzz_interconnect
 import re
 
-cfg = FuzzConfig(job="TAPROUTE", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_PLC_R11C14:TAP_PLC"])
+configs = [
+    ([(11, 7), (11, 19)], FuzzConfig(job="TAPROUTE", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_PLC_R11C14:TAP_PLC"])),
+    ([(10, 7), (10, 19)], FuzzConfig(job="TAPROUTECIB", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_CIB_R10C14:TAP_CIB"])),
+    ([(1, 7), (1, 19)], FuzzConfig(job="TAPROUTECIBT", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_CIBT_R1C14:TAP_CIBT"])),
+    ([(11, 80)], FuzzConfig(job="TAPROUTE_1S", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_PLC_1S_R11C74:TAP_PLC_1S"])),
+    ([(10, 80)], FuzzConfig(job="TAPROUTECIB_1S", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_CIB_1S_R10C74:TAP_CIB_1S"])),
+    ([(1, 80)], FuzzConfig(job="TAPROUTECIBT_1S", device="LIFCL-40", sv="../shared/route_40.v", tiles=["TAP_CIBT_1S_R1C74:TAP_CIBT_1S"])),
+]
 
 def main():
-    cfg.setup()
-    nodes = ["R11C7_HPBX{:02}00".format(i) for i in range(8)] + ["R11C19_HPBX{:02}00".format(i) for i in range(8)]
-    fuzz_interconnect(config=cfg, nodenames=nodes, regex=False, bidir=False, full_mux_style=True)
+    for locs, cfg in configs:
+        cfg.setup()
+        nodes = []
+        for r, c in locs:
+            nodes += ["R{}C{}_HPBX{:02}00".format(r, c, i) for i in range(8)]
+        fuzz_interconnect(config=cfg, nodenames=nodes, regex=False, bidir=False, full_mux_style=True)
 
 if __name__ == "__main__":
     main()
