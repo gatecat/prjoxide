b11759aeb54023ec48e057fe5bbb47b3c8fd7284
David Shah <dave@ds0.me>
2020-01-02 17:49:39 +0000

    Run EBR routing fuzzer in all locations
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/LIFCL/061-ebr-routing/fuzzer.py
+++ fuzzers/LIFCL/061-ebr-routing/fuzzer.py
@@ -7,14 +7,22 @@ configs = [
         "cfg": FuzzConfig(job="EBRROUTE0", device="LIFCL-40", sv="../shared/route_40.v", tiles=["CIB_R29C15:EBR_1", "CIB_R29C16:EBR_2", "CIB_R29C14:MIB_EBR"]),
         "rc": (28, 14),
     },
+    {
+        "cfg": FuzzConfig(job="EBRROUTE1", device="LIFCL-40", sv="../shared/route_40.v", tiles=["CIB_R29C18:EBR_4", "CIB_R29C19:EBR_5", "CIB_R29C17:MIB_EBR"]),
+        "rc": (28, 17),
+    },
+    {
+        "cfg": FuzzConfig(job="EBRROUTE2", device="LIFCL-40", sv="../shared/route_40.v", tiles=["CIB_R29C21:EBR_7", "CIB_R29C22:EBR_8", "CIB_R29C23:EBR_9", "CIB_R29C24:EBR_10", "CIB_R29C20:MIB_EBR"]),
+        "rc": (28, 20),
+    },
+    {
+        "cfg": FuzzConfig(job="EBRROUTE3", device="LIFCL-40", sv="../shared/route_40.v", tiles=["CIB_R29C23:EBR_9", "CIB_R29C21:EBR_7", "CIB_R29C22:EBR_8", "CIB_R29C24:EBR_10", "CIB_R29C20:MIB_EBR"]),
+        "rc": (28, 22),
+    },
 ]
 
 ignore_tiles = set([
-    "CIB_R28C14:CIB",
-    "CIB_R28C15:CIB",
-    "CIB_R28C16:CIB",
-    "CIB_R28C17:CIB",
-    "CIB_R28C18:CIB",
+    "CIB_R28C{}:CIB".format(c) for c in range(14, 27)
 ])
 
 def main():
