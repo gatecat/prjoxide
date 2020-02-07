e18937fff87869d1a53504248cacbf26de5e1872
David Shah <dave@ds0.me>
2020-02-07 16:15:07 +0000

    Add 'copy IO' meta-fuzzer
    Signed-off-by: David Shah <dave@ds0.me>

---
+++ fuzzers/LIFCL/039-copy-io/fuzzer.py
@@ -0,0 +1,18 @@
+import database
+import libprjoxide
+
+def main():
+    db = libprjoxide.Database(database.get_db_root())
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B5_1", ["SYSIO_B5_1_V18"], "PEWC", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B4_0", ["SYSIO_B4_0_DQS1", "SYSIO_B4_0_DQS3", "SYSIO_B4_0_DLY50", "SYSIO_B4_0_DLY42"], "PEWC", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B4_1", ["SYSIO_B4_1_DQS0", "SYSIO_B4_1_DQS2", "SYSIO_B4_1_DQS4", "SYSIO_B4_1_DLY52"], "PEWC", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B3_0", ["SYSIO_B3_0_DLY30_V18", "SYSIO_B3_0_DQS1", "SYSIO_B3_0_DQS3"], "PEWC", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B3_1", ["SYSIO_B3_1_DLY32", "SYSIO_B3_1_DQS0", "SYSIO_B3_1_DQS2", "SYSIO_B3_1_DQS4", "SYSIO_B3_1_ECLK_R", "SYSIO_B3_1_V18"], "PEWC", "")
+
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B1_0", ["SYSIO_B1_0_C"], "C", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B2_0", ["SYSIO_B2_0_C"], "C", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B6_0", ["SYSIO_B6_0_C"], "C", "")
+    libprjoxide.copy_db(db, "LIFCL", "SYSIO_B7_0", ["SYSIO_B7_0_C"], "C", "")
+
+if __name__ == '__main__':
+    main()
