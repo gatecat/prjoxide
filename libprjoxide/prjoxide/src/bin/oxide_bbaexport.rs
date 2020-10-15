c84ebce956ad842d9a7cc510c7470322c4299850
David Shah <dave@ds0.me>
2020-10-15 12:09:37 +0100

    bbaexport: Improved nextpnr integration
    Signed-off-by: David Shah <dave@ds0.me>

--- libprjoxide/prjoxide/src/bin/oxide_bbaexport.rs
+++ libprjoxide/prjoxide/src/bin/oxide_bbaexport.rs
@@ -8,35 +8,48 @@ use prjoxide::database::*;
 
 use std::iter::FromIterator;
 
+use std::fs::File;
+
 use std::io::{BufWriter, Result};
 
 fn main() -> Result<()> {
     let exe_path = std::env::current_exe()?;
     let args = Vec::from_iter(std::env::args());
-    let mut ids = IdStringDB::from_constids(&args[1])?;
+
+    if args.len() != 4 {
+        panic!("Usage: oxide_bbaexport family constids.inc out.bba");
+    }
+
+    let family = &args[1];
+    let mut ids = IdStringDB::from_constids(&args[2])?;
+    let outfile = File::create(&args[3])?;
+
+
+    if family != "LIFCL" {
+        // TODO: multiple family and device support
+        panic!("unsupported family {}", &family);
+    }
+
     let db_path = exe_path.parent().unwrap().join("../../../database");
     let db_path_str = db_path.to_str().unwrap();
     let mut db = Database::new(db_path_str);
+
+
     let tts = TileTypes::new(&mut db, &mut ids, "LIFCL", "LIFCL-40");
     let empty_chip = Chip::from_name(&mut db, "LIFCL-40");
     let mut lgrid = LocationGrid::new(&empty_chip, &mut db, &tts);
     lgrid.stamp_neighbours();
     let mut lts = LocationTypes::from_locs(&mut lgrid);
     lts.import_wires(&mut ids, &tts);
-    /*
-    for y in 0..lgrid.height {
-        for x in 0..lgrid.width {
-            print!("{},", lgrid.get(x, y).unwrap().type_at_loc.unwrap());
-        }
-        println!("");
-    }
-    */
-    let mut bba_str = BufWriter::new(std::io::stdout());
+
+    let mut bba_str = BufWriter::new(outfile);
     let mut bba = BBAWriter::new(&mut bba_str);
     bba.pre("#include \"nextpnr.h\"")?;
+    bba.pre("#include \"embed.h\"")?;
     bba.pre("NEXTPNR_NAMESPACE_BEGIN")?;
+    bba.post(&format!("EmbeddedFile chipdb_file_{0}(\"nexus/chipdb-{0}.bin\", chipdb_blob_{0});", &family))?;
     bba.post("NEXTPNR_NAMESPACE_END")?;
-    bba.push("db_blob")?;
+    bba.push(&format!("chipdb_blob_{}", &family))?;
     bba.ref_label("db")?;
 
     let mut bba_s = BBAStructs::new(&mut bba);
