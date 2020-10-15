d7099611b70e4797d3420a848c9fdfbdfcf22740
David Shah <dave@ds0.me>
2020-10-15 18:46:26 +0100

    Add examples folder
    Signed-off-by: David Shah <dave@ds0.me>

---
+++ examples/common.mk
@@ -0,0 +1,32 @@
+# Common Makefile for Nexus examples
+
+# *** Remove these flags as nextpnr-nexus support improves
+SYNTH_ARGS=-flatten -noccu2 -nolutram -nowidelut -nobram
+
+YOSYS?=yosys
+NEXTPNR?=nextpnr-nexus
+PRJOXIDE?=prjoxide
+ECPPROG?=ecpprog
+
+all: $(PROJ).bit
+
+$(PROJ).json: $(PROJ).v $(EXTRA_VERILOG) $(MEM_INIT_FILES) 
+	$(YOSYS) -ql $(PROJ)_syn.log -p "synth_nexus $(SYNTH_ARGS) -top top -json $(PROJ).json" $(PROJ).v $(EXTRA_VERILOG)
+
+$(PROJ).fasm: $(PROJ).json $(PDC)
+	$(NEXTPNR) --device $(DEVICE) --pdc $(PDC) --json $(PROJ).json --fasm $(PROJ).fasm
+
+$(PROJ).bit: $(PROJ).fasm
+	$(PRJOXIDE) pack $(PROJ).fasm $(PROJ).bit
+
+prog: $(PROJ).bit
+	$(ECPPROG) -S $(PROJ).bit
+
+prog-flash: $(PROJ).bit
+	$(ECPPROG) $(PROJ).bit
+
+clean:
+	rm -f $(PROJ).json $(PROJ).fasm $(PROJ)_syn.log $(PROJ).bit
+
+.SECONDARY:
+.PHONY: prog prog-flash clean
