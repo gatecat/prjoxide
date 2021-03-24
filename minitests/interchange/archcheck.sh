eda3e796719f1961dc188f921c222911f63789bb
gatecat <gatecat@ds0.me>
2021-03-24 10:50:34 +0000

    interchange: Update to track upstream
    Signed-off-by: gatecat <gatecat@ds0.me>

--- minitests/interchange/archcheck.sh
+++ minitests/interchange/archcheck.sh
@@ -8,6 +8,6 @@ NEXTPNR_BUILD=../../../nextpnr/build
 mkdir -p work
 prjoxide interchange-export ${DEVICE} work/${DEVICE}.capnp
 python3 -m fpga_interchange.nextpnr_emit --schema_dir ../../3rdparty/fpga-interchange-schema/interchange \
-	--output_dir work --device work/${DEVICE}.capnp --bel_bucket_seeds nexus_bel_buckets.yaml
+	--output_dir work --device work/${DEVICE}.capnp --device_config nexus_device_config.yaml
 ${NEXTPNR_BUILD}/bba/bbasm --l work/chipdb.bba work/${DEVICE}.bin
 ${NEXTPNR_BUILD}/nextpnr-fpga_interchange --chipdb work/${DEVICE}.bin --test
