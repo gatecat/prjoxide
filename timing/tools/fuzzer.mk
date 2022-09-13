8131499cd409cbcaf4b2a8b53a7a12902e4811f9
Maciej Kurc <mkurc@antmicro.com>
2022-09-13 12:43:43 +0200

    Isolated LRAM fuzzing to the new fuzzer 09-lram, added dumping LRAM types and configurations to JSON files
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- timing/tools/fuzzer.mk
+++ timing/tools/fuzzer.mk
@@ -1,3 +1,5 @@
+PYTHON ?= python3
+
 all: work/stamp
 
 work/stamp: $(foreach run,$(RUNS),work/$(run).stamp)
@@ -6,9 +8,17 @@ work/stamp: $(foreach run,$(RUNS),work/$(run).stamp)
 BASE_DIR=../../../../
 OUT_DIR=../../../output/
 
+ifneq ($(GENERATOR),)
 work/design_%.v: $(GENERATOR)
 	mkdir -p work/
-	python $(GENERATOR) > $@
+	$(PYTHON) $(GENERATOR) > $@
+endif
+
+ifneq ($(GENERATOR2),)
+work/design_%.v: $(GENERATOR2)
+	mkdir -p work/
+	$(PYTHON) $(GENERATOR2) --vlog $@ --conf $(subst .v,.json,$@)
+endif
 
 work/design_%.bit: work/design_%.v
 	$(BASE_DIR)/radiant.sh $(DEVICE) $< || $(ALLOW_FAIL)
@@ -16,7 +26,7 @@ work/design_%.bit: work/design_%.v
 work/%.stamp: work/design_%.bit
 	mkdir -p $(OUT_DIR)
 
-	python $(BASE_DIR)/timing/util/extract_route.py work/design_$*.tmp/par.udb $(OUT_DIR)/$(NAME)_$*_route.pickle
+	$(PYTHON) $(BASE_DIR)/timing/util/extract_route.py work/design_$*.tmp/par.udb $(OUT_DIR)/$(NAME)_$*_route.pickle
 
 	# we need the '|| true' because of
 	# malloc_consolidate(): unaligned fastbin chunk detected
@@ -30,6 +40,8 @@ work/%.stamp: work/design_%.bit
 	$(BASE_DIR)/radiant_cmd.sh backanno -sp 12 -w -o $(OUT_DIR)/$(NAME)_$*_12.vo work/design_$*.tmp/par.udb || true
 
 	$(BASE_DIR)/radiant_cmd.sh backanno -min -w -o $(OUT_DIR)/$(NAME)_$*_M.vo work/design_$*.tmp/par.udb || true
+
+	[ -f "work/design_$*.json" ] && cp -f "work/design_$*.json" $(OUT_DIR)/$(NAME)_$*.json
 
 	touch $@
 
